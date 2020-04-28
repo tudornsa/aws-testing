0fdd9f77c4f444323d27357db2637cd80d08b622
Tudor Ioneasa <t.ioneasa@levi9.com>
2020-04-28 18:05:09 +0300

    moved buildspec files to resources folder

--- infra/deploy-infrastructure.sh
+++
@@ -1,7 +0,0 @@
-echo "Validating cloudformation template..."
-aws cloudformation validate-template --template-body file://./infra/infrastructure-template.yaml
-
-echo "Deploying cloudformation template..."
-aws cloudformation deploy --template-file ./infra/infrastructure-template.yaml --stack-name InfraStack
-
-read -p "Completed. Press any key to exit"
